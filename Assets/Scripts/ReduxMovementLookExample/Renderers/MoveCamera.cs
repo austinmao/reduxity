f64d01e42f4576d0965f904a79238cce141cd855
Austin Mao <austinmao@MacBook.local>
2016-12-26 19:05:51 +0100

    WIP: Apply rendering code, now need to test

--- Assets/Scripts/ReduxMovementLookExample/Renderers/MoveCamera.cs
+++ Assets/Scripts/ReduxMovementLookExample/Renderers/MoveCamera.cs
@@ -20,26 +20,10 @@ namespace Reduxity.Example.PlayerMovementLook {
             // Debug.Log($"App.Store: {App.Store}");
             App.Store
                 .Subscribe(state => {
-                    // Debug.Log($"going to move character by: {distance}");
-                    // camera_.transform.localRotation = state.Look.lookRotation;
+                    Debug.Log($"looking by rotation: {state.Camera.transform.localRotation}");
+                    camera_.transform.localRotation = state.Camera.transform.localRotation;
                 })
                 .AddTo(this);
         }
-
-        // Ripped straight out of the Standard Assets MouseLook script. (This should really be a standard function...)
-		private Quaternion ClampRotationAroundXAxis(Quaternion q, float minAngle, float maxAngle) {
-			q.x /= q.w;
-			q.y /= q.w;
-			q.z /= q.w;
-			q.w = 1.0f;
-
-			float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-
-			angleX = Mathf.Clamp(angleX, minAngle, maxAngle);
-
-			q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-			return q;
-		}
     }
 }
