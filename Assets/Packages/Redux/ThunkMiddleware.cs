5df958bd829adb38c2d8f1d3b64e74852b9909fc
Austin Mao <austinmao@MacBook.local>
2016-12-28 11:27:42 +0100

    (!) Adding packages
    Packages was in gitignore

---
+++ Assets/Packages/Redux/ThunkMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// from https://github.com/taiste/redux.NET-thunk
+namespace Redux.Thunk
+{
+    public static class Middleware
+    {
+        public static Func<Dispatcher, Dispatcher> ThunkMiddleware<TState>(IStore<TState> store)
+        {
+            return (Dispatcher next) => (IAction action) =>
+            {
+                var thunkAction = action as ThunkAction<TState>;
+                if (thunkAction != null) {
+                    thunkAction.Action(store.Dispatch, store.GetState);
+                    return thunkAction;
+                }
+                return next(action);                
+            };
+        }
+    }
+
+    public class ThunkAction<TState>: IAction
+    {
+        public Action<Dispatcher, Func<TState>> Action { get; }
+
+        public ThunkAction(Action<Dispatcher, Func<TState>> action)
+        {
+            this.Action = action;
+        }
+    }
+}
+
---
+++ Assets/Packages/Reduxity/IReducer.cs
@@ -0,0 +1,4 @@
+namespace Reduxity {
+    public interface IReducer {
+    }
+}
