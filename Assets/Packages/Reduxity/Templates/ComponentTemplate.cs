0d2d6c6717db7a8cde6f2a56564e4e5fceb5a92c
Austin Mao <austin@austinmao.com>
2017-01-06 17:54:42 +0100

    Move templates to their on folder

---
+++ Assets/Packages/Reduxity/Templates/ComponentTemplate.cs
@@ -0,0 +1,21 @@
+﻿// using Zenject;
+// using UnityEngine.UI;
+// using System;
+// using Redux;
+
+// namespace Reduxity {
+
+//     public class Component : IComponent {
+
+// 		readonly Dispatcher dispatch_;
+
+//         public Component(
+// 			App app
+//         ) {
+// 			dispatch_ = app.Store.Dispatch;
+//         }
+
+// 		public void Render() {
+// 		}
+//     }
+// }
---
+++ Assets/Packages/Reduxity/Templates/ObserverTemplate.cs
@@ -0,0 +1,34 @@
+﻿// using UnityEngine;
+// using Zenject;
+// // using System;
+
+// namespace Reduxity {
+
+// 	/// <summary>
+//     /// Reference template for observers that are attached to a GameObject
+// 	/// and bound via Zenject binding script
+//     /// </summary>
+// 	public class ObserverTemplate : MonoBehaviour, IInitializable {
+// 		App app_;
+// 		// Settings settings_;
+
+// 		[Inject]
+// 		public void Construct(
+// 			App app
+// 			// Settings settings
+// 		) {
+// 			app_ = app;
+// 			// settings_ = settings;
+// 		}
+
+// 		public void Initialize() {
+// 			Debug.Log($"ObserverTemplate initialized with store: {ObjectDumper.Dump(app_.Store)}");
+// 		}
+
+// 		// [Serializable]
+// 		// /// <summary>
+// 		// /// </summary>
+// 		// public class Settings {
+// 		// }
+// 	}
+// }
