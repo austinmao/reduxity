821be73b94ec677c5b5174ab5ab09cc8868e3d3a
Austin Mao <austin@austinmao.com>
2017-01-06 20:34:20 +0100

    Added client and lobby action creator

--- Assets/Scripts/ReduxZenjectExample/Redux/ActionCreators/Network/ClientConnectCreator.cs
+++ Assets/Scripts/ReduxZenjectExample/Redux/ActionCreators/Network/ClientConnectCreator.cs
@@ -19,11 +19,16 @@ namespace Reduxity.Example.Zenject.ClientConnectCreator {
         public IAction Connect(Action.Connect action) {
             // return thunk to store, which will dispatch new actions upon success or failure
             return new ThunkAction<State> ((dispatch, getState) => {
-                // dispatch first action to set state to loading
-                dispatch(new CloudConnector.Action.ConnectStart {});
+                // dispatch first action to set state to connecting
+                dispatch(new ClientConnector.Action.ConnectStart {});
 
-                // connect to PhotonCloud. Callbacks are handled by Photon.
-				// PhotonNetwork.ConnectUsingSettings(action.settings);
+                // TODO: add this if ever used
+                // connect to client. Callbacks are handled by Photon.
+                /// <param name="masterServerAddress">The server's address (either your own or Photon Cloud address).</param>
+                /// <param name="port">The server's port to connect to.</param>
+                /// <param name="appID">Your application ID (Photon Cloud provides you with a GUID for your game).</param>
+                /// <param name="gameVersion">This client's version number. Users are separated by gameversion (which allows you to make breaking changes).</param>
+                // PhotonNetwork.ConnectToMaster(masterServerAddress, port, appID, gameVersion);
             });
         }
 
@@ -34,8 +39,10 @@ namespace Reduxity.Example.Zenject.ClientConnectCreator {
         public IAction DisconnectStart(Action.Disconnect action) {
             // return thunk to store, which will dispatch new actions upon success or failure
             return new ThunkAction<State> ((dispatch, getState) => {
-                // dispatch first action to set state to loading
-                // dispatch(new RoomConnector.Action.JoinStart {});
+                dispatch(new ClientConnector.Action.DisconnectStart {});
+
+                // TODO: add this if ever used
+                // PhotonNetwork.Disconnect();
             });
         }
     }
--- Assets/Scripts/ReduxZenjectExample/Redux/ActionCreators/Network/LobbyConnectCreator.cs
+++ Assets/Scripts/ReduxZenjectExample/Redux/ActionCreators/Network/LobbyConnectCreator.cs
@@ -6,8 +6,10 @@ namespace Reduxity.Example.Zenject.LobbyConnectCreator {
         /// <summary>
         /// Proprties for each type of http request
         /// </summary>
-        public class Connect : IAction {}
-        public class Disconnect : IAction {}
+        public class Join : IAction {
+            public TypedLobby lobby;
+        }
+        public class Leave : IAction {}
     }
 
     public class ActionCreator : IActionCreator {
@@ -15,14 +17,20 @@ namespace Reduxity.Example.Zenject.LobbyConnectCreator {
         /// Thunk that connects to Photon Network
         /// </summary>
         /// <returns>ThunkAction for on success or failure handlers</returns>
-        public IAction Connect(Action.Connect action) {
+        public IAction Connect(Action.Join action) {
             // return thunk to store, which will dispatch new actions upon success or failure
             return new ThunkAction<State> ((dispatch, getState) => {
                 // dispatch first action to set state to loading
                 dispatch(new LobbyConnector.Action.JoinStart {});
 
-                // connect to PhotonLobby. Callbacks are handled by Photon.
-				// PhotonNetwork.ConnectUsingSettings(action.settings);
+                // join default lobby if lobby is not specified
+                if (action.lobby == null) {
+                    PhotonNetwork.JoinLobby();
+
+                // else join specified lobby
+                } else {
+                    PhotonNetwork.JoinLobby(action.lobby);
+                }
             });
         }
 
@@ -30,11 +38,14 @@ namespace Reduxity.Example.Zenject.LobbyConnectCreator {
         /// Thunk that disconnects from Photon Network
         /// </summary>
         /// <returns>ThunkAction for on success or failure handlers</returns>
-        public IAction DisconnectStart(Action.Disconnect action) {
+        public IAction DisconnectStart(Action.Leave action) {
             // return thunk to store, which will dispatch new actions upon success or failure
             return new ThunkAction<State> ((dispatch, getState) => {
-                // dispatch first action to set state to loading
-                // dispatch(new RoomConnector.Action.JoinStart {});
+                // dispatch first action to set state to leaving
+                dispatch(new LobbyConnector.Action.LeaveStart {});
+
+                // leave PhotonLobby. Callbacks are handled by Photon.
+				PhotonNetwork.LeaveLobby();
             });
         }
     }
