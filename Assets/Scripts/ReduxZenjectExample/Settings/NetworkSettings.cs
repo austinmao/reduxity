d2b50b9cac56a3bfe31cfaeee593eb2564347841
Austin Mao <austin@austinmao.com>
2017-01-06 21:05:17 +0100

    Organize observers into folders

--- Assets/Scripts/ReduxZenjectExample/Settings/NetworkSettings.cs
+++ Assets/Scripts/ReduxZenjectExample/Settings/NetworkSettings.cs
@@ -2,15 +2,13 @@
 using Zenject;
 
 namespace Reduxity.Example.Zenject {
-	public class NetworkSettings {
 
-		[Serializable]
-		public class GameSettings {
+	[Serializable]
+	public class PhotonSettings {
 
-            /// <summary>
-            /// This client's version number. Users are separated from each other by gameversion (which allows you to make breaking changes).
-            /// </summary>
-			public string GameVersion;
-		}
+		/// <summary>
+		/// This client's version number. Users are separated from each other by gameversion (which allows you to make breaking changes).
+		/// </summary>
+		public string GameVersion;
 	}
 }
