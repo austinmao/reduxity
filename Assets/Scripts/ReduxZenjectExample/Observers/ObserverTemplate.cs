58d2d4c9235efc67031a67cf47d96bbd58c6c60e
Austin Mao <austin@austinmao.com>
2017-01-06 11:58:09 +0100

    Add Observer template for reference

---
+++ Assets/Scripts/ReduxZenjectExample/Observers/ObserverTemplate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Zenject;
+using System;
+
+namespace Reduxity.Example.Zenject {
+	public class ObserverTemplate : MonoBehaviour, IInitializable {
+		App app_;
+		// Settings settings_;
+
+		[Inject]
+		public void Construct(
+			App app
+			// Settings settings
+		) {
+			app_ = app;
+			// settings_ = settings;
+		}
+
+		public void Initialize() {
+			Debug.Log($"ObserverTemplate initialized ith store: {app_.Store}");
+		}
+
+		// [Serializable]
+		// /// <summary>
+		// /// </summary>
+		// public class Settings {
+		// }
+	}
+}
